ff4d85193fb7f26c67965ccdbb999533e857a903
Unuku05 <143775909+Unuku05@users.noreply.github.com>
2025-09-08 23:51:06 -0700

    sv

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About(){
+    return(
+        <div>
+        <h1>
+            Tale of R8 Bot
+        </h1>
+        <p>
+            Playing with R8 Revolver in CS2.
+        </p>
+        <p>He is bot in every game</p>
+        </div>
+    )
+}
